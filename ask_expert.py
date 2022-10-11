1d7e559082c754804aa834ac8d55ded75b2e0bf6
Elyor Dusyor Ugli Rasulov <112889202+EleeForDSandAI@users.noreply.github.com>
2022-10-12 05:10:53 +0900

    Add files via upload

--- ask_expert.py
+++ ask_expert.py
@@ -14,17 +14,17 @@ def read_from_file():
 
 def write_to_file(country_name, city_name):
     with open('capitals_data.txt', 'a') as file:
-        file.write('\n', country_name + '/' + city_name)
+        file.write('\n' + country_name + '/' + city_name)
 
 read_from_file()
 
 while True:
     query_country = simpledialog.askstring('Country', 'Type the name of a country:')
-    if query_country in the_world:
-        result = the_world[query_country]
-        messagebox.showinfo('Answer', 'The capital city of ' + query_country + ' is ' + result + '!')
+    if query_country.capitalize() in the_world:
+        result = the_world[query_country.capitalize()]
+        messagebox.showinfo('Answer', 'The capital city of ' + query_country.capitalize() + ' is ' + result + '!')
     else:
-        new_city = simpledialog.askstring('Teach me', 'I don\' know!' + 'What is the capital city of ' + query_country + '?')
-        the_world[query_country] = new_city
-        write_to_file(query_country, new_city)
+        new_city = simpledialog.askstring('Teach me', 'I don\' know!' + 'What is the capital city of ' + query_country.capitalize() + '?')
+        the_world[query_country.capitalize()] = new_city.capitalize()
+        write_to_file(query_country.capitalize(), new_city.capitalize())
 root.mainloop()
